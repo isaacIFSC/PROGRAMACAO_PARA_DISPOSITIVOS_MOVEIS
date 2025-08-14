0877c263cbbf7c857a3816359f7854eb608c126e
IsaacIFSC <isaac.c17@aluno.ifsc.edu.br>
2025-08-13 22:15:41 -0300

    app contador

--- app/src/main/java/isaac/app/MainActivity.java
+++ app/src/main/java/isaac/app/MainActivity.java
@@ -1,6 +1,8 @@
 package isaac.app;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    int i = 0;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+        tv.setText(Integer.toString(i));
+        tv.setOnClickListener(v -> {
+            i++;
+            tv.setText(Integer.toString(i));
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
