8d1247d59ddeb0076da2ccd9840490b4f5f15487
isaacIFSC <isaac.c17@aluno.ifsc.edu.br>
2025-08-20 20:49:39 -0300

    log android studio

--- app/src/main/java/isaac/app/MainActivity.java
+++ app/src/main/java/isaac/app/MainActivity.java
@@ -1,6 +1,7 @@
 package isaac.app;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Log.d("ciclo_vida", "onCreate");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("ciclo_vida", "onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("ciclo_vida", "onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("ciclo_vida", "onPause");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("ciclo_vida", "onRestart");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("ciclo_vida", "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("ciclo_vida", "onDestroy");
     }
 }
+
