66fd01b7a753310a2579d25efdc88d97290c08af
isaacIFSC <isaac.c17@aluno.ifsc.edu.br>
2025-12-02 23:17:02 -0300

    sensoooor

--- app/src/main/java/isaac/app/MainActivity.java
+++ app/src/main/java/isaac/app/MainActivity.java
@@ -1,55 +1,51 @@
 package isaac.app;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    SensorManager sm;
+    Sensor sensorLux, sensorPressure;
+    TextView tvLux, tvPressure;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Log.d("ciclo_vida", "onCreate");
-    }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d("ciclo_vida", "onStart");
-    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d("ciclo_vida", "onResume");
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d("ciclo_vida", "onPause");
+        tvLux = findViewById(R.id.textView);
+        tvPressure = findViewById(R.id.textView2);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorLux = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorPressure = sm.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        sm.registerListener(this, sensorLux, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, sensorPressure, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d("ciclo_vida", "onRestart");
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+
+        if(sensorType == 6) {
+            float pressure = event.values[0];
+            tvPressure.setText("pressure " + pressure);
+        } else {
+            float lux = event.values[0];
+            tvLux.setText("lux " + lux);
+
+        }
     }
 
     @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d("ciclo_vida", "onStop");
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d("ciclo_vida", "onDestroy");
     }
 }
-
